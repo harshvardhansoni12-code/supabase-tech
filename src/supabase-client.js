7d1e25ceec2360122de9ee90afaff69efa7367b3
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-04-19 11:56:51 +0530

    changes in supabase-client

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -3,14 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    storage: {
-      getItem: () => {
-        return Promise.resolve("FETCHED_COOKIE");
-      },
-      setItem: () => {},
-      removeItem: () => {},
-    },
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseKey);
